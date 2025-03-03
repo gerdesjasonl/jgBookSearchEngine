182d1fc578ab056ae93845dd1028329e73953d5d
gerdesjasonl <gerdesjasonl@gmail.com>
2025-03-03 15:36:18 -0500

    Completed queries.ts and mutations.ts according to rubric

---
+++ client/src/utils/mutations.ts
@@ -0,0 +1,56 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($input: UserInput!) {
+        login(input: $input) { 
+        token
+        user {
+            _id
+            username
+            email
+            }
+        }
+    }`;
+
+export const ADD_USER = gql`
+    mutation addUser($input: UserInput!) {
+        addUser(input: $input) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: BookInput!) {
+        saveBook(book: $book) {
+            _id
+            username
+            savedBooks {
+                bookId
+                title
+                authors
+                description
+                image
+                link
+            }
+        }
+    }`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _idusername
+            savedBooks {
+                bookId
+                title
+                authors
+                description
+                image
+                link
+            }
+        }
+    }`;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import db from './config/connection';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { GraphQLContext } from './utils/context';
@@ -10,16 +11,23 @@ import mongoose from 'mongoose';
 
 dotenv.config()
 
-const app = express();
-
 const server = new ApolloServer({
   typeDefs, 
   resolvers,
 });
 
-app.use(
-  '/graphql',
-  expressMiddleware(server, {
+const startApolloServer = async () => {
+
+  await server.start();
+  await db();
+
+  const PORT = process.env.PORT || 3001;
+  const app = express();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server, {
     context: async ({ req, res }): Promise<GraphQLContext> => {
       const authHeader = req.headers.authorization || '';
       const token = authHeader.split(' ')[1] || '';
@@ -43,11 +51,46 @@ app.use(
       }
 
       return { req, res, user };
-    }
-  })
-);
+    }}));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+};
 
-server.start().then(() => {
-  app.listen(4000, () => console.log('Server running on http://localhost:4000/graphql'));
-});
+startApolloServer();
+
+// app.use(
+//   '/graphql',
+//   expressMiddleware(server, {
+//     context: async ({ req, res }): Promise<GraphQLContext> => {
+//       const authHeader = req.headers.authorization || '';
+//       const token = authHeader.split(' ')[1] || '';
+//       let user = null;
+
+//       if (token) {
+//         try {
+//           const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+//           const userDoc = await User.findById(decoded._id);
+
+//           if (userDoc && userDoc._id instanceof mongoose.Types.ObjectId) {
+//             user = {
+//               _id: userDoc._id.toString(), 
+//               username: userDoc.username, 
+//               email: userDoc.email, 
+//             };
+//           }
+//         } catch (err) {
+//           console.error('Invalid token:', err);
+//         }
+//       }
+
+//       return { req, res, user };
+//     }
+//   })
+// );
+
+// server.start().then(() => {
+//   app.listen(3001, () => console.log('Server running on http://localhost:3001/graphql'));
+// });
 
